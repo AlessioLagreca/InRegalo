3411b6f7ec5ab4ad4b736d9d3f5bef3813a4dd2e
Alessio Lagreca <82826676+AlessioLagreca@users.noreply.github.com>
2024-06-15 13:10:27 +0200

    started building search-bar and main buttons

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Container from "@/components/wrappers/container";
 import { Button } from "@/components/ui/button";
 import MieiAnnunci from "@/components/MieiAnnunci";
+import { Input } from "@/components/ui/input";
 
 export default async function ProtectedPage() {
 	const supabase = createClient();
@@ -19,7 +20,7 @@ export default async function ProtectedPage() {
 
 	return (
 		<Container>
-			<div className='flex flex-col flex-1 gap-20 items-center w-full'>
+			<div className='flex flex-col flex-1 gap-8 items-center w-full'>
 				<div className='w-full'>
 					<div className='py-3 font-bold text-center bg-gradient-to-r from-green-100 to-green-200'>
 						Benvenuto in InRegalo, il posto dove gestire i tuoi regali
@@ -40,12 +41,68 @@ export default async function ProtectedPage() {
 
 				{/* SEZIONE ANNUNCI */}
 
-				<div className='flex flex-col flex-1 gap-20 px-3 max-w-4xl opacity-01 animate-in'>
-					<Link prefetch={false} href='/nuovo_annuncio'>
-						<Button className='bg-green-500 hover:bg-green-600 font-bold'>
-							Inserisci annuncio
-						</Button>
-					</Link>
+				<div className='flex flex-col flex-1 justify-center items-center gap-20 px-3 max-w-4xl opacity-01 animate-in'>
+					<div className='p-4 border-2 border-zinc-200 rounded-md w-full'>
+						<div className='flex gap-4 w-full'>
+							<Link
+								prefetch={false}
+								href='/nuovo_annuncio'
+								className='flex flex-1'
+							>
+								<Button
+									variant='outline'
+									size='xl'
+									className='flex-1 w-full text-green-500 hover:text-green-600 border-green-500 hover:bg-green-100 font-bold '
+								>
+									Inserisci annuncio
+								</Button>
+							</Link>
+							<Link
+								prefetch={false}
+								href='/nuovo_annuncio'
+								className='flex flex-1'
+							>
+								<Button
+									size='xl'
+									variant='outline'
+									className='flex-1 text-green-500 hover:text-green-600 border-green-500 hover:bg-green-100 font-bold'
+								>
+									Preferiti
+								</Button>
+							</Link>
+							<Link
+								prefetch={false}
+								href='/nuovo_annuncio'
+								className='flex flex-1'
+							>
+								<Button
+									size='xl'
+									variant='outline'
+									className='flex-1 text-green-500 hover:text-green-600 border-green-500 hover:bg-green-100 font-bold'
+								>
+									Messaggi
+								</Button>
+							</Link>
+							<Link
+								prefetch={false}
+								href='/nuovo_annuncio'
+								className='flex flex-1'
+							>
+								<Button
+									size='xl'
+									variant='outline'
+									className='flex-1 text-green-500 hover:text-green-600 border-green-500 hover:bg-green-100 font-bold'
+								>
+									Miei annunci
+								</Button>
+							</Link>
+						</div>
+					</div>
+					<Input
+						type='text'
+						placeholder='Cerca annunci'
+						className='border-2 border-zinc-300 rounded-3xl max-w-2xl w-full py-6 text-lg focus:outline-none focus:ring-2 focus-visible:outline-none focus:ring-gray-300'
+					/>
 					<MieiAnnunci id={user?.id} />
 					<p>Hello {user?.id}</p>
 				</div>
